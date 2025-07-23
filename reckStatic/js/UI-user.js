fc8dd52362a478218fed226f72bd50b57f97d622
Shubhaam-p <shubhamgpatil03@gmail.com>
2025-07-23 22:22:41 +0530

    Changes for fixing cache issue

--- reckStatic/js/UI-user.js
+++ reckStatic/js/UI-user.js
@@ -5,7 +5,7 @@ async function listGalleryMediaFiles(calledAgain = false, page = 0){
         pagination = `&page=${page}`;
 
     let res = await makeAjaxCall({
-        url: `${webURL}/new-cont-reg?Type=Gall3ry&action=listMedia&home=1${pagination}`,
+        url: `${webURL}/new-cont-reg?Type=Gall3ry&action=listMedia&home=1${pagination}&ts=${Date.now()}`,
         method: "GET",
         });
     return res; 
